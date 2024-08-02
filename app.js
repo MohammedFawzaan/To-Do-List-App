f5158ff5f450b3a246c8dcca9ead9c11e8fad6b6
Mohammed Fawzaan <fawzaanmd31@gmail.com>
2024-08-02 15:26:51 +0530

    Project Updated

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const inputbox = document.getElementById("inputbox");
 const list = document.getElementById("listcontainer");
-const button = document.querySelector('button');
-button.addEventListener('click',function() {
+const button = document.querySelector('.btn');
+const light = document.querySelector('#light');
+const button1 = document.getElementById('light');
+
+button1.addEventListener('click', function() {
+    document.body.classList.toggle('dark-bg');
+    document.body.classList.toggle('light-bg');
+});
+
+function addTask() {
     if(inputbox.value === '') {
         alert('Write a task');
     } else {
@@ -16,24 +24,13 @@ button.addEventListener('click',function() {
         inputbox.value = '';
         save();
     }
-});
-document.addEventListener('keypress',function addtask(event){
-    if(event.key === "Enter" || event.keyCode === 13) 
-    {
-        if(inputbox.value === '') {
-            alert('Write a task');
-        } else {
-            let li = document.createElement('li');
-            li.innerText = inputbox.value;
-            list.appendChild(li);
-    
-            let span = document.createElement('span');
-            span.innerHTML = "\u00d7";
-            li.appendChild(span);
-    
-            inputbox.value = '';
-            save();
-        }
+}
+
+button.addEventListener('click', addTask);
+
+document.addEventListener('keypress', function(event){
+    if(event.key === "Enter" || event.keyCode === 13) {
+        addTask();
     }
 });
 
@@ -47,6 +44,7 @@ list.addEventListener('click', function(event) {
     }
 });
 
+// Saving Data in Local Storage
 function save() {
     localStorage.setItem("data", list.innerHTML);
 }
@@ -54,4 +52,5 @@ function save() {
 function showTask() {
     list.innerHTML = localStorage.getItem('data');
 }
+
 showTask();
